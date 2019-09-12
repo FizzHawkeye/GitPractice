13a20a8306c904fd9bfd5d9616f0983e1b52ca4a
yujian.liu <yujian.liu@unisoc.com>
2019-09-12 16:22:17 +0800

    Add Feature: Multiplication

--- AddMinusMultiDiv/AddMinusMultiDiv/main.c
+++ AddMinusMultiDiv/AddMinusMultiDiv/main.c
@@ -2,6 +2,7 @@
 int main(){
 	printf("Helloworld\n");
 	int result = 0;
+	result = myMulti(4, 5);
 	printf("Result: %d \n", result);
 	return 0;
 }
---
+++ AddMinusMultiDiv/AddMinusMultiDiv/multi.c
@@ -0,0 +1,3 @@
+int myMulti(int a, int b){
+	return a*b;
+}

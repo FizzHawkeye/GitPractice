8c247193affa30abde156f84ca66af634fb9772e
yujian.liu <yujian.liu@unisoc.com>
2019-09-12 16:07:26 +0800

    Add Feature: Minus

--- AddMinusMultiDiv/AddMinusMultiDiv/main.c
+++ AddMinusMultiDiv/AddMinusMultiDiv/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 int main(){
 	printf("Helloworld\n");
+	int result = 0;
+	result = myMinus(6,4);
+	printf("Result: %d \n", result);
 	return 0;
 }
---
+++ AddMinusMultiDiv/AddMinusMultiDiv/minus.c
@@ -0,0 +1,3 @@
+int myMinus(int a, int b){
+	return a - b;
+}

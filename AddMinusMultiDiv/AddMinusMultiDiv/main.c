1ceace95224e4ba1df642d2bfb4a479a7046ab02
yujian.liu <yujian.liu@unisoc.com>
2019-09-12 15:54:38 +0800

    Add Feature: Addition

--- AddMinusMultiDiv/AddMinusMultiDiv/main.c
+++ AddMinusMultiDiv/AddMinusMultiDiv/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+//int myAdd(int a, int b); Unnecessary attach here
 int main(){
 	printf("Helloworld\n");
+	int result = 0;
+	result = myAdd(6, 5);
+	printf("Result: %d\n", result);
 	return 0;
 }

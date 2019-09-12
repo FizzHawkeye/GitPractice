74d47e659def0f2a937b25cdd9e9c90685f27fa0
yujian.liu <yujian.liu@unisoc.com>
2019-09-12 16:35:57 +0800

    Add Feature: Fake Division

---
+++ AddMinusMultiDiv/AddMinusMultiDiv/division.cpp
@@ -0,0 +1,7 @@
+int myDiv(int a, int b)
+{
+	if (b == 0)
+		return -1;	
+	else
+		return -1;
+}
--- AddMinusMultiDiv/AddMinusMultiDiv/main.c
+++ AddMinusMultiDiv/AddMinusMultiDiv/main.c
@@ -2,6 +2,7 @@
 int main(){
 	printf("Helloworld\n");
 	int result = 0;
+	result = 8 / 4;
 	printf("Result: %d \n", result);
 	return 0;
 }
